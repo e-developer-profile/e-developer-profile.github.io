a4d81fccc617d3f94a41b1b2adfb37c079b19f13
E. Junior <evando66.ea@gmail.com>
2023-02-07 00:50:51 -0300

    feat: different color everyday

--- profile.js
+++ profile.js
@@ -1,5 +1,15 @@
-const changeBodyColor = ()=>{
-    console.log('changeBodyColor');
+const changeElementsColors = ()=>{
+    const body = document.getElementsByTagName('body')[0]
+    const day = new Date().getDay()
+    const month = new Date().getMonth() + 1
+    const year = Number(new Date().getFullYear().toString().slice(1))
+    const bodyTodayColor = `rgb(${month},${day},${year})`
+    body.style.backgroundColor = bodyTodayColor
+
+    const everydayElement = document.getElementById('everyday-color')
+    const everydayTodayColor = `rgb(${day},${year},${month})`
+    everydayElement.style.color = everydayTodayColor
+    
 }
 
-changeBodyColor()
+changeElementsColors()
