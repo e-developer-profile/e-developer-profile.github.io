ae310adf94ce2adf86318d5b2a32d1e800f6c1d0
E. Junior <evando66.ea@gmail.com>
2023-02-08 00:35:22 -0300

    fix: how to get date day

--- profile.js
+++ profile.js
@@ -1,6 +1,6 @@
 const changeElementsColors = ()=>{
     const body = document.getElementsByTagName('body')[0]
-    const day = new Date().getDay()
+    const day = new Date().getDate()
     const month = new Date().getMonth() + 1
     const year = Number(new Date().getFullYear().toString().slice(1))
     const bodyTodayColor = `rgb(${year},${month},${day})`
